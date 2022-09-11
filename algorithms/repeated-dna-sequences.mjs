be540c0272ffb872215d58292d22bc288138672d
billowstao <22577367+billowstao@users.noreply.github.com>
2022-09-11 18:50:23 +0800

    feat(algorithms): 更新滑动窗口-187.重复的DNA序列

--- algorithms/repeated-dna-sequences.mjs
+++ algorithms/repeated-dna-sequences.mjs
@@ -1,47 +1,41 @@
 /**
  * 187. 重复的DNA序列
  *
- * @see {@link https://leetcode.cn/problems/repeated-dna-sequences}
+ * @see {@link https://leetcode.cn/problems/repeated-dna-sequences/}
  */
 
 /**
  * @param {string} s
  * @return {string[]}
  */
-const findRepeatedDnaSequences = function (s) {
-  const SUB_STR_LEN = 10;
-  const CHAR_BIT_LEN = 2;
-  const charBit = new Map([
+ const findRepeatedDnaSequences = function (s) {
+  const strLen = s.length;
+  const subStrLength = 10;
+  const charMap = new Map([
     ['A', 0],
     ['C', 1],
     ['G', 2],
     ['T', 3],
   ]);
-  const result = [];
-  const strLen = s.length;
-
-  if (SUB_STR_LEN >= strLen) {
-    return result;
-  }
-
+  const CHAR_BITS = 2;
+  const subStrNum = new Map();
   let subStrBits = 0;
 
-  // index 边界 -1，将第一次窗口滑动纳入处理流程
-  for (let index = 0; index < SUB_STR_LEN - 1; ++index) {
-    subStrBits = (subStrBits << CHAR_BIT_LEN) | charBit.get(s[index]);
+  for (let index = 0; index < subStrLength - 1; ++index) {
+    const char = s[index];
+    subStrBits = (subStrBits << CHAR_BITS) | charMap.get(char);
   }
 
-  const subStrTime = new Map();
+  const result = [];
 
-  for (let index = 0; index <= strLen - SUB_STR_LEN; index++) {
-    // index 边界 -1，将第一次窗口滑动纳入处理流程
+  for (let index = subStrLength - 1; index < strLen; ++index) {
     subStrBits =
-      ((subStrBits << CHAR_BIT_LEN) | charBit.get(s[index + SUB_STR_LEN - 1])) &
-      ((1 << (SUB_STR_LEN * CHAR_BIT_LEN)) - 1);
-    subStrTime.set(subStrBits, (subStrTime.get(subStrBits) ?? 0) + 1);
+      ((subStrBits << CHAR_BITS) | charMap.get(s[index])) &
+      ((1 << (subStrLength * CHAR_BITS)) - 1);
+    subStrNum.set(subStrBits, (subStrNum.get(subStrBits) ?? 0) + 1);
 
-    if (subStrTime.get(subStrBits) === 2) {
-      result.push(s.slice(index, index + SUB_STR_LEN));
+    if (subStrNum.get(subStrBits) === 2) {
+      result.push(s.slice(index - subStrLength + 1, index + 1));
     }
   }
 
