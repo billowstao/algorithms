2d9d16af3f0bd163ae75152eae55ae3dde4ee611
billowstao <22577367+billowstao@users.noreply.github.com>
2022-09-12 18:28:44 +0800

    feat(algorithms): 滑动窗口-209.长度最小的子数组

---
+++ algorithms/minimum-size-subarray-sum.mjs
@@ -0,0 +1,42 @@
+/**
+ * 209. 长度最小的子数组
+ *
+ * @see {@link https://leetcode.cn/problems/minimum-size-subarray-sum/}
+ */
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minSubArrayLen = function (target, nums) {
+  const numbersAmount = nums.length;
+  let right = -1;
+  let sum = 0;
+  let minLen = 0;
+
+  for (let index = 0; index < numbersAmount; ++index) {
+    if (index) {
+      sum -= nums[index - 1];
+    }
+
+    while (right + 1 < numbersAmount && sum < target) {
+      sum += nums[right + 1];
+      ++right;
+    }
+    
+    if (sum >= target) {
+      const len = right - index + 1;
+      if (!minLen) {
+        minLen = len;
+      } else if (len < minLen) {
+        minLen = len;
+      }
+    }
+  }
+
+  return minLen;
+};
+
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+console.log(minSubArrayLen(4, [1, 4, 4]));
