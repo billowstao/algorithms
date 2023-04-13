e46d956564ed2aaa59bc012627f9acc57f4825e1
billowstao <22577367+billowstao@users.noreply.github.com>
2023-04-13 23:34:47 +0800

    feat(data-structure): linked-list
    - 新增链表 插入、删除、查找、删除头节点、删除尾节点 方法
    - 更新 pnpm 版本至 v8，更新 `pnpm-lock.yaml`

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -34,7 +34,7 @@ export class LinkedList {
   }
 
   /**
-   * 向前添加节点
+   * 向链表头部插入节点
    *
    * @param {*} value - 值
    * @returns {LinkedList} 链表
@@ -71,5 +71,172 @@ export class LinkedList {
     this.tail = newNode;
 
     return this;
+  }
+
+  /**
+   * 插入节点到指定索引
+   *
+   * @param {*} value - 值
+   * @param {number} rawIndex - 索引
+   * @returns {LinkedList} 链表
+   */
+  insert(value, rawIndex) {
+    const index = rawIndex >= 0 ? rawIndex : 0;
+
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      let count = 1;
+      let currentNode = this.head;
+      const newNode = new LinkedListNode(value);
+
+      while (currentNode) {
+        if (count === index) {
+          break;
+        }
+
+        currentNode = currentNode.next;
+        ++count;
+      }
+
+      if (currentNode) {
+        // 插入指定索引之后
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+      } else {
+        // 尾部插入节点
+        if (this.tail) {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        } else {
+          this.head = newNode;
+          this.tail = newNode;
+        }
+      }
+    }
+
+    return this;
+  }
+
+  /**
+   * 删除节点
+   *
+   * @param {*} value
+   * @returns {(LinkedListNode|null)} 删除的节点
+   */
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+    // 如果需要删除的节点为头节点，删除至下一个不相同的节点，并且将此节点变为新的头节点
+    while (this.head && this.compare.equal(this.head.value, value)) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode) {
+      while (currentNode.next) {
+        if (this.compare.equal(currentNode.next.value, value)) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+
+    // 处理尾指针需要删除的情况
+    if (this.compare.equal(this.tail.value, value)) {
+      this.tail = currentNode;
+    }
+
+    return deletedNode;
+  }
+
+  /**
+   * 查找节点
+   *
+   * @param {object} props - 查找参数
+   * @param {*} props.value - 值
+   * @param {function} [props.callback] - 查找回调函数
+   */
+  find({ value, callback }) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (typeof callback === 'function' && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      if (this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  /**
+   * 删除尾节点
+   *
+   * @returns {LinkedListNode} 删除的节点
+   */
+  deleteTail() {
+    const deletedTail = this.tail;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        // 从当前节点向前判断 2 个节点，遍历到尾节点的前 1 个节点
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+
+    return deletedTail;
+  }
+
+  /**
+   * 删除头节点
+   *
+   * @returns {LinkedListNode} 删除的节点
+   */
+  deleteHead() {
+    const deletedHead = this.head;
+
+    if (!this.head) {
+      return deletedHead;
+    }
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
   }
 }
